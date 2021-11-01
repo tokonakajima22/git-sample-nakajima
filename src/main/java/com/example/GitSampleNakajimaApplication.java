a270b7c4ade05043038a071865fe003a49e96923
tokonakajima22 <toko.nakajima@rakus-partners.co.jp>
2021-11-01 14:27:51 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleNakajimaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNakajimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNakajimaApplication.class, args);
+	}
+
+}
